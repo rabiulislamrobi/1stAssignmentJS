279966f096eea0635572a62387c4732c696806d2
rabiulislalmrobi <rabiulislamrobi31443161@gmail.com>
2023-11-13 12:32:26 +0600

    added assignment

--- assignment.js
+++ assignment.js
@@ -1,1 +1,62 @@
 // assignment
+// ১. Variable কি ?
+variable keywords,variable short form var ,variable er amra jekono valid nam dite pari ,kicu rules follow kore 1
+1.Letter(a to z and A to Z)
+2.Digit(0 to 9)
+3.underscore _ 
+4.$ 
+variable name casesensative 
+// ২. Variable কিভাবে লিখতে হয় ?
+variable var keywords diye likhte hoy
+var firstName="robi"
+eikhane var er keywords a robi store kora
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+string:double cottasion er vitore ja thake thai string ,cottasion er vitore kicu na thakle o string empty string 
+var name="robi";
+
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+Number:cotassion bade jodi digit thake tahole ta number data types
+var number=10;
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+boolean: boolean er resul true or false hoye thake , true hole 1 and false hole 0
+var boolean=true;
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+var names="rabiul";
+document.write(names.toUpperCase);
+var names2="RABIUL";
+document.write(names2.toLowerCase);
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+operator :
+two types of operator 
+1.arithmetic operator :+,-,*,/,%,**
+2.assign oeperator ;=
+
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+
+var number=4.5555;
+document.write(Math.abs(number));
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+
+var number=4.000001;
+document.write(Math.ceil(number));
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+var number =8.99;
+document.write(Math.floor(number));
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+var number=8.9;
+document.write(Math.round());
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+ var number=Math.random();
+ document.write(number);
+ amra jodi 100 er mode randomm number ber korte chai tahole
+  var number =Math.random()*100;
+  document.write(number);
+// asignment.js - GitHub Link 
